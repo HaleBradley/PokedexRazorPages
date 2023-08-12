d361d5a7c3d41169b06601f8a6d93643e03615fc
Hale Bradley <hbradley787@gmail.com>
2023-08-12 17:25:47 -0500

    Edit to Pokemon Model, removed fields not in Api

--- Pages/Pokemon.cshtml.cs
+++ Pages/Pokemon.cshtml.cs
@@ -1,35 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Elfie.Extensions;
+using PokedexRazorPages.Models;
+using PokedexRazorPages.Services;
+using static PokedexRazorPages.Models.Pokemon;
 
 namespace PokedexRazorPages.Pages
 {
     public class PokemonModel : PageModel
     {
-        public string Name { get; set; }
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public List<string> Abilities { get; set; }
         public string Image { get; set; }
-        public string Description { get; set; }
-        public string Height { get; set; }
-        public string Category { get; set; }
-        public double Weight { get; set; }
-        public string[] Abilities { get; set; }
-        public string Gender { get; set; }
-        public string[] Types { get; set; }
-        public string[] Weaknesses { get; set; }
+        public List<string> Types { get; set; }
+        public string Api { get; set; }
 
-        public void OnGet(string? mon)
+        private readonly PokeApi _pokeApi;
+
+        public PokemonModel(PokeApi pokeApi)
         {
-            Name = "Bulbasaur";
-            Id = 0001;
-            Image = "~/images/bulbasaur.png";
-            Description = "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.";
-            Height = "2'04\"";
-            Category = "Seed";
-            Weight = 15.2;
-            Abilities = new string[] { "Overgrow" };
-            Gender = "♂ ♀";
-            Types = new string[] { "Grass", "Poison" };
-            Weaknesses = new string[] {"Fire", "Psychic", "Flying", "Ice"};
+            _pokeApi = pokeApi;
+        }
+
+        public async Task OnGetAsync(string? name)
+        {
+            if (name != null)
+            {
+                try
+                {
+                    Pokemon pokemonData = await _pokeApi.GetPokemonData(name);
+                    Id = pokemonData.Id;
+                    Name = pokemonData.Name;
+                    Height = pokemonData.Height;
+                    Weight = pokemonData.Weight;
+                    Abilities = new List<string>();
+                    foreach (var abilityObj in pokemonData.Abilities)
+                    {
+                        Abilities.Add(abilityObj.Ability.Name);
+                    }
+                    Image = pokemonData.Sprites.Sprite.Art.Image;
+                    Types = new List<string>();
+                    foreach (var typeObj in pokemonData.Types)
+                    {
+                        Types.Add(typeObj.Type.Name);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Api = "Bad Request";
+                }
+            }
         }
     }
 }
---
+++ Services/PokeApi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PokedexRazorPages.Models;
+
+namespace PokedexRazorPages.Services
+{
+    
+    public class PokeApi
+    {
+        private readonly HttpClient _httpClient;
+
+        public PokeApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+        }
+
+        public async Task<Pokemon> GetPokemonData(string pokemonName)
+        {
+            string requestUrl = $"pokemon/{pokemonName}";
+            Console.WriteLine("Request URL: " + requestUrl);
+
+            HttpResponseMessage response = await _httpClient.GetAsync(requestUrl);
+            Console.WriteLine("Response Status Code: " + response.StatusCode);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonResponse);
+                Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(jsonResponse); 
+                Console.WriteLine("Returning obj");
+                return pokemon;
+            } 
+            else
+            {
+                throw new Exception($"Failed to fecth data from PokeAPI. Status Code: {response.StatusCode}");
+            }
+        }
+    }
+}
