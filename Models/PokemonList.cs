c11bd3370a549a7798e50adfb707a1d81cacb9c2
Hale Bradley <hbradley787@gmail.com>
2023-08-15 09:18:28 -0500

    Index page now displays pokemon info from api.

---
+++ Models/PokemonList.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace PokedexRazorPages.Models
+{
+    public class PokemonListResponse
+    {
+        [JsonProperty("results")]
+        public List<PokemonResult> Results { get; set; }
+    }
+
+    public class PokemonResult
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PokedexRazorPages.Models;
+using PokedexRazorPages.Services;
 
 namespace PokedexRazorPages.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PokeApi _pokeApi;
+        public List<string> PokemonNames { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, PokeApi pokeApi)
         {
             _logger = logger;
+            _pokeApi = pokeApi;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            try
+            {
+                PokemonNames = await _pokeApi.GetPokemonNamesList();
+                Pokemons = new List<Pokemon>();
+                foreach (string name in PokemonNames)
+                {
+                    Pokemon pokemonTemp = await _pokeApi.GetPokemonData(name);
+                    Pokemons.Add(pokemonTemp);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Bad request {ex}");
+            }
         }
     }
 }
